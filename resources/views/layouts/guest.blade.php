f1855fd4b197ed5bbc18bb81f8740af232580704
Kashif <ahamed.kashif@northsouth.edu>
2023-04-05 16:53:21 +0600

    vite removed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
@@ -26,5 +25,6 @@
                 {{ $slot }}
             </div>
         </div>
+        <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
     </body>
 </html>
